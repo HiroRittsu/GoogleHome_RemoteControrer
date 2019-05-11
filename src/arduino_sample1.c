59584c8692e00a6b554da086988a90c0cf9e418e
MiglyA <zq6295migly@gmail.com>
2019-05-11 13:06:26 +0900

    [update]defineを削除、出力bit数を入力情報に依存させた

--- src/arduino_sample1.c
+++ src/arduino_sample1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-
-#define HEX_NUMBER 25
-#define BIT_NUMBER 100
-#define DIVISOR "10001"
+#include <stdlib.h>
 
 /*
-
 16進数のコードから
 ２進数変換
 誤り検知符号
 raw_data変換
-
 */
 
+int size(char* array) {
+	int count = 0;
+	while (*array++ != '\0') count++;
+	return count;
+}
 
 char* int2bin(int x) {
 	static char bin[4];
@@ -25,10 +25,9 @@ char* int2bin(int x) {
 
 void convert(char* hexs, char* bits) {
 	char table[] = "0123456789abcdef";
-	int ints[HEX_NUMBER];
-
+	int* ints = (int *)malloc(size(hexs) * sizeof(int));
 	//16進数から10進数に変換
-	for (int i = 0; i < HEX_NUMBER; ++i) {
+	for (int i = 0; i < size(hexs); ++i) {
 		for (int t = 0; t < 16; ++t) {
 			if (hexs[i] == table[t]) {
 				ints[i] = t;
@@ -37,7 +36,7 @@ void convert(char* hexs, char* bits) {
 		}
 	}
 	//10進数から2進数に変換
-	for (int i = 0; i < HEX_NUMBER; ++i) {
+	for (int i = 0; i < size(hexs); ++i) {
 		char* tmp = int2bin(ints[i]);
 		for (int c = 0; c < 4; ++c) {
 			*bits++ = tmp[c];
@@ -45,43 +44,38 @@ void convert(char* hexs, char* bits) {
 	}
 }
 
-void calc_error_detection(char* bits, char* error) {
-
-	char result_bits[BIT_NUMBER];
-
-	for (int i = 0; i < BIT_NUMBER; ++i) result_bits[i] = bits[i];
-
-	for (int divisor_target = 0; divisor_target < BIT_NUMBER - 4; ++divisor_target) {
-
+void calc_error_detection(char* bits, char* error, char* divisor) {
+	char* result_bits = (char *)malloc(size(bits) * sizeof(char));
+	for (int i = 0; i < size(bits); ++i) result_bits[i] = bits[i];
+	for (int divisor_target = 0; divisor_target < size(bits) - 4; ++divisor_target) {
 		if (result_bits[divisor_target] == '0') continue;
-
-		for (int i = 0; i < BIT_NUMBER; ++i) {
+		for (int i = 0; i < size(bits); ++i) {
 			if (i >= divisor_target && i < divisor_target + 5) {
-				if (result_bits[i] == DIVISOR[i - divisor_target])
+				if (result_bits[i] == divisor[i - divisor_target])
 					result_bits[i] = '0';
 				else
 					result_bits[i] = '1';
 			}
 		}
 	}
-
-	for (int i = 0; i < 4; ++i) error[i] = result_bits[BIT_NUMBER - 4 + i];
+	for (int i = 0; i < 4; ++i) error[i] = result_bits[size(bits) - 4 + i];
 }
 
 
-void add_error_detection(char* bits) {
-
+void add_error_detection(char* bits, char* divisor) {
 	char error_detection[4];
-
-	calc_error_detection(bits, error_detection);
-
-	for (int i = 0; i < 4; ++i) bits[BIT_NUMBER + i] = error_detection[i];
+	calc_error_detection(bits, error_detection, divisor);
+	for (int i = 0; i < 5; ++i) {
+		bits[size(bits)] = error_detection[i];
+		bits[size(bits) + 1] = '\0';
+	}
 }
 
-void generate_ir_data(unsigned int* rawData, char* bits, int* times) {
+unsigned int* generate_ir_data(char* bits, int* times) {
+	unsigned int* rawData = (unsigned int*)malloc(((size(bits) + 2) * 2 - 1) * sizeof(unsigned int));
 	*rawData++ = times[0];
 	*rawData++ = times[1];
-	for (int i = 0; i < BIT_NUMBER + 4; ++i) {
+	for (int i = 0; i < size(bits); ++i) {
 		*rawData++ = times[2];
 		if (bits[i] == '0')
 			*rawData++ = times[3];
@@ -89,26 +83,27 @@ void generate_ir_data(unsigned int* rawData, char* bits, int* times) {
 			*rawData++ = times[4];
 	}
 	*rawData = times[2];
+	rawData -= ((size(bits) + 2) * 2 - 2);
+	return rawData;
 }
 
-void aircon_data(char* hexs, unsigned int  *rawData) {
-	//printf("%d\n", (int)strlen(hexs));
-	static char bits[BIT_NUMBER + 4];
+unsigned int*  aircon_data(char* hexs) {
+	char* bits = (char *)malloc((size(hexs) * 4 + 4) * sizeof(char));
 	int times[] = {3750, 2050, 300, 600, 1600};
+	unsigned int* rawData;
+
 	convert(hexs, bits);
-	add_error_detection(bits);
-	generate_ir_data(rawData, bits, times);
+	add_error_detection(bits, "10001");
+	rawData = generate_ir_data(bits, times);
+	printf("%s\n", bits );
+	return rawData;
 }
 
 int main(int argc, char const *argv[]) {
-
-	unsigned int  rawData[211];
-
-	aircon_data("aaa5fc0110112200800a004e1", rawData);
-
+	unsigned int*  rawData;
+	rawData = aircon_data("aaa5fc0110112200800a004e1");
 	for (int i = 0; i < 211; ++i) {
 		printf("%d,", rawData[i] );
 	}
-
 	return 0;
 }
