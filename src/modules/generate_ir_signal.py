82679dc63236b820012ca6c030dcdf94e62a1e6f
HiroRittsu <zq1012noza@gmail.com>
2023-12-17 22:24:03 +0900

    [wip]温度設定中

--- src/modules/generate_ir_signal.py
+++ src/modules/generate_ir_signal.py
@@ -19,14 +19,20 @@ class GenerateIrSignal:
 
     def sharp_air_conditioner(self, temperature: int, mode: str) -> list:
         result_data = []
+        result_data.extend([9000, 4500])
         if temperature == -1:
-            result_data.extend([9000, 4500])
             result_data.extend(self.covert_str_bit("00110000000100000000011000000011111"))
             result_data.extend([650, 20000])
             result_data.extend(self.covert_str_bit("00000000000000010000000000001101"))
 
         else:
-            pass
+            bin_v1 = format(~(31 - temperature) & 0b1111, '04b')[::-1]
+            bin_v2 = format(~(28 - temperature) & 0b1111, '04b')[::-1]
+            result_data.extend(self.covert_str_bit(f"00110000{bin_v1}00000000011000000011111"))
+            result_data.extend([650, 20000])
+            result_data.extend(self.covert_str_bit(f"0000000000000001000000000000{bin_v2}"))
+            print(bin_v1)
+            print(bin_v2)
 
         result_data.extend([650, 40000])
         result_data.extend([9000, 4500])
@@ -39,7 +45,7 @@ class GenerateIrSignal:
 
 
 if __name__ == '__main__':
-    print(GenerateIrSignal().sharp_air_conditioner(-1, ""))
+    print(GenerateIrSignal().sharp_air_conditioner(24, ""))
     # print(len(GenerateIrSignal().sharp_air_conditioner(-1, "")))
     encoded_raw_data = GenerateIrSignal().rle(np.asarray(GenerateIrSignal().sharp_air_conditioner(-1, "")))
     print(
