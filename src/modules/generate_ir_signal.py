e96819b18129d8efdf0ec1f22e641faee7bb7dbd
HiroRittsu <zq1012noza@gmail.com>
2023-12-18 21:44:47 +0900

    [wip]エアコン切断確認

--- src/modules/generate_ir_signal.py
+++ src/modules/generate_ir_signal.py
@@ -21,25 +21,24 @@ class GenerateIrSignal:
         result_data = []
         result_data.extend([9000, 4500])
         if temperature == -1:
-            result_data.extend(self.covert_str_bit("00110000000100000000011000000011111"))
+            result_data.extend(self.covert_str_bit("00100000000100000000010000000011111"))
             result_data.extend([650, 20000])
-            result_data.extend(self.covert_str_bit("00000000000000010000000000001101"))
+            result_data.extend(self.covert_str_bit("00000000000000010000000000001100"))
+            result_data.extend([650, 40000])
+            result_data.extend([9000, 4500])
+            result_data.extend(self.covert_str_bit("00000000000000000000000000000111111"))
+            result_data.extend([650, 20000])
+            result_data.extend(self.covert_str_bit("00000000000000000000000000001111"))
+            result_data.append(650)  # 終了ビット
 
         else:
             bin_v1 = format(~(31 - temperature) & 0b1111, '04b')[::-1]
             bin_v2 = format(~(28 - temperature) & 0b1111, '04b')[::-1]
             result_data.extend(self.covert_str_bit(f"00110000{bin_v1}00000000011000000011111"))
             result_data.extend([650, 20000])
-            result_data.extend(self.covert_str_bit(f"0000000000000001000000000000{bin_v2}"))
-            print(bin_v1)
-            print(bin_v2)
-
-        result_data.extend([650, 40000])
-        result_data.extend([9000, 4500])
-        result_data.extend(self.covert_str_bit("00000000000000000000000000000111111"))
-        result_data.extend([650, 20000])
-        result_data.extend(self.covert_str_bit("00000000000000000000000000001111"))
-        result_data.append(650)  # 終了ビット
+            result_data.extend(self.covert_str_bit(f"0000000000100001000000100000{bin_v2}"))
+            print(f"00110000{bin_v1}00000000011000000011111")
+            print(f"0000000000100001000000100000{bin_v2}")
 
         return result_data
 
@@ -49,4 +48,4 @@ if __name__ == '__main__':
     # print(len(GenerateIrSignal().sharp_air_conditioner(-1, "")))
     encoded_raw_data = GenerateIrSignal().rle(np.asarray(GenerateIrSignal().sharp_air_conditioner(-1, "")))
     print(
-        "{\"raw_data\":" + f"{list(encoded_raw_data[0])}" + ", \"length\": " + f"{list(encoded_raw_data[1])}" + "}")
+        "{\"khz\":38,\"base_time\":650,\"negative_time\":550,\"positive_time\":1600,\"comp_array\":" + f"{list(encoded_raw_data[0])}" + ", \"length_array\": " + f"{list(encoded_raw_data[1])}" + "}")
